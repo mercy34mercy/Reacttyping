6a7334083eeb98fd7765402f238b24f2ecd2a3d5
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2022-03-28 14:36:31 +0900

    firstcommit#

---
+++ src/Test.tsx
@@ -0,0 +1,13 @@
+import './Test.css';
+
+function Test() {
+    return (
+        <div className='Test'>
+            <header className="Test-header">
+                これはテストです
+            </header>
+        </div>
+    );
+}
+
+export default Test;
