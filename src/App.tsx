dc032c241cb607362f70e5f8090a2135ad7e8eda
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2022-03-30 13:11:50 +0900

    取り合えず正解判定はできた

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,87 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+type counter = {
+  count: number
+}
+
+const Game = () => {
+  return (
+    <Inputbar></Inputbar>
+  )
+}
+
+const Inputbar = () => {
+  const question: string[] = ["watnow", "gutti"]
+  const [counter, setCounter] = React.useState(0);
+  const [questioncounter, setQuestioncounter] = React.useState(0);
+  const [inputValue, setInputValue] = React.useState("");
+  const [answerstring,setAnswer] = React.useState("")
+  const questionlength:number = question[questioncounter].length
+
+  const handlechenge = (event:any) => {
+    setInputValue(event.target.value);
+    judgement(event.target.value)
+  };
+
+  const judgement = (value: string) => {
+    
+
+    const len = value.length
+
+    if (question[questioncounter][counter] === value[len - 1]) {
+      setCounter(counter + 1)
+      setAnswer(answerstring + value[len - 1])
+      console.log("正解")
+    }
+
+    console.log("q", question[questioncounter].length,"\nc",counter)
+    if (question[questioncounter].length === counter) {
+      setCounter(0)
+      setQuestioncounter(questioncounter + 1)
+      setAnswer("")
+      console.log("reset")
+    }
+  }
+
+
+
+
+  return (
+    <div className='inputbar'>
+      <input value={inputValue} onChange={handlechenge}></input>  
+      <Textoutput text={answerstring} num={counter} questionnum={questionlength}></Textoutput>
+    </div>
+  )
+}
+
+
+type Props = {
+  text: string
+  num: number
+  questionnum:number
+}
+
+const Textoutput = (props: Props) => {
+  const [str, setstr] = React.useState("")
+  
+  useEffect(() => {
+    setstr(props.text)
+    if (props.num === props.questionnum) {
+      setstr("")
+    }
+  });
+
+  return (
+    <div className='textoutput'>
+      <p>{str}</p>
+      <p>{props.num}</p>
+    </div>
+  )
+
+}
 
 function App() {
   return (
@@ -23,4 +104,4 @@ function App() {
   );
 }
 
-export default App;
+export default Game;
--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Test from './Test'
 import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import Game from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test></Test>
+    <Game />
   </React.StrictMode>,
-  document.getElementById('test')
+  document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Test></Test>
+//   </React.StrictMode>,
+//   document.getElementById('test')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
