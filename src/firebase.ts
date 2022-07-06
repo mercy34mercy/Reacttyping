a1bba1642f6fb6759d229eb3ab1001ca7f07ab8e
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-07-06 17:54:40 +0900

    継続的にログインさせる処理とログイン周りのCSS完了

--- src/firebase.ts
+++ src/firebase.ts
@@ -17,4 +17,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth();
+const fireauth = getAuth();
+
+export const firebaseApp = {
+    fireauth
+}
--- src/start.tsx
+++ src/start.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './start.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from './firebase';
+import { firebaseApp } from "./firebase"
+import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider, Auth, AuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
@@ -11,33 +11,11 @@ provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
 
 
-signInWithPopup(auth, provider)
-    .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-    }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-    });
-
-const handleclick = () => {
-    const auth = getAuth()
-    signInWithPopup(auth,provider)
-}
 
 
 export const Startview = () => {
     const navigate = useNavigate();
+
 
     useEffect(() => {
         document.addEventListener("keydown", keyFunction, false)
@@ -59,9 +37,7 @@ export const Startview = () => {
                 </div>
             </div>
             <div className="pc-dsp">
-                <div className='login'>
-                    <p className='loginbutton' onClick={handleclick}>ログイン</p>
-                </div>
+                <Login></Login>
                 <div className='manuebar'>
                     <p><img className='kanban' src="./kanban.png"></img></p>
                     {/* <p><Link to="game"><img className='startButton' src="./start.png"></img></Link></p> */}
@@ -72,4 +48,73 @@ export const Startview = () => {
             </div>
         </div>
     )
+}
+
+export const Login = () => {
+    const [loginflag, setloginflag] = React.useState(false)
+    const [auth, setauth] = React.useState(firebaseApp.fireauth)
+    const [username, setuser] = React.useState<string | null>("")
+
+    useEffect(() => {
+        checklogin(auth)
+    },[])
+
+    const checklogin = (auth:Auth) => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                setloginflag(true)
+                setuser(user.displayName)
+                // ...
+            } else {
+                // User is signed out
+                // ...
+            }
+        });
+    }
+
+    const popup = (auth: Auth, provider: AuthProvider) => {
+        signInWithPopup(auth,provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential?.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user)
+            setuser(user.displayName)
+            setloginflag(true)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            console.log(errorCode)
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            // The email of the user's account used.
+            // const email = error.customData.email;
+            // The AuthCredential type that was used.
+            // const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        })};
+    const handleclick = () => {
+        popup(auth,provider)
+    }
+
+    if (loginflag) {
+        return (
+            <div className='displayname'>
+                <button className='logout'>ログアウト</button>
+                <p className='name'>ようこそ{username}さん</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className='login'>
+                <button className='loginbutton' onClick={handleclick}>ログイン</button>
+            </div>
+        )
+    }
 }
