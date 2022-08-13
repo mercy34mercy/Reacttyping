5965b7f97c3fea0b58f03ab4a3c14127b3150f17
mercy34mercy <makun@DESKTOP.localdomain>
2022-08-13 13:37:39 +0900

    順位、名前、スコアの画像完了

--- src/Ranking/Ranking.tsx
+++ src/Ranking/Ranking.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { RankingBox } from "./RankingBox";
 import { FetchRanking } from "../Firebase/FetchRanking";
+import "./Ranking.css"
 
 export const Ranking = () => {
 
     const { rankingdata, loadflag } = FetchRanking()
 
         return (
-            <div>
+            <div className="ranking">
             <RankingBox rankdata={rankingdata} flag={loadflag} />
             </div>
         )
